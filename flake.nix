50f42e5a547882bfe98f4e8c4235351fff11acc8
haruki7049 <tontonkirikiri@gmail.com>
2024-09-20 23:59:15 +0900

    hoge

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    buildJanetPackage = {
+      url = "github:haruki7049/buildJanetPackage";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+      perSystem =
+        { pkgs, ... }:
+        let
+          janet-clap = pkgs.callPackage ./. { buildJanetPackage-source = inputs.buildJanetPackage; };
+        in
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.nixfmt.enable = true;
+            programs.shellcheck.enable = true;
+            programs.actionlint.enable = true;
+
+            settings.formatter.shellcheck.excludes = [
+              ".envrc"
+            ];
+          };
+
+          checks = {
+            inherit janet-clap;
+          };
+
+          packages = {
+            inherit janet-clap;
+            default = janet-clap;
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = [
+              # Nix
+              pkgs.nil
+
+              # Janet
+              pkgs.janet
+              pkgs.jpm
+            ];
+          };
+        };
+    };
+}
